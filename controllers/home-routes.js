d94417c2f9f1d9f893bb1cda02d40d02985d3762
Adam Smith <smith.adam937@gmail.com>
2022-08-03 15:39:02 -0500

    WIP controllers home-routes, index, and withAuth.js added

---
+++ controllers/home-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router(); 
+
+
+
+
+// Need to confirm the names of the models requiring in 
+const { Post, User, Comment, Vote } = require ('../models');
---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router(); 
+
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+// need to create this: 
+const withAuth = require('../utils/auth'); 
+
+
+
+
+
+
+
+
+
+module.exports = router; 
