fe5520fca223aa7112571d076912fa7df5b5889d
lundbmp <m.lundberg008@gmail.com>
2022-08-04 14:18:27 -0500

    added a price value to Post Model and adjusted the seed data to reflect that

--- models/Downvote.js
+++ models/Downvote.js
@@ -1,40 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Downvote extends Model {}
 
 Downvote.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'post',
-                key: 'id'
-            }
-        }
-
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamp: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'downvote'
-    }
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamp: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "downvote",
+  }
 );
 
 module.exports = Downvote;
