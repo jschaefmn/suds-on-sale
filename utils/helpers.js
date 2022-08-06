cf6ad72eba64b8f5a67aef790671fa87bea074a9
lundbmp <m.lundberg008@gmail.com>
2022-08-06 09:27:27 -0500

    added helper functions for jake

---
+++ utils/helpers.js
@@ -0,0 +1,20 @@
+const helpers = {
+    // retrieving the month, day, and year from a date being passed in
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+  format_url: (url) => {
+    // trimming urls down to foo.com
+    return url
+      .replace("http://", "")
+      .replace("https://", "")
+      .replace("www.", "")
+      .split("/")[0]
+      .split("?")[0];
+  },
+};
+
+module.exports = helpers;
+
