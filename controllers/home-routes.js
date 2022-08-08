20fc3e66943d4f48684ee2df20438d1f4154956f
lundbmp <m.lundberg008@gmail.com>
2022-08-07 19:11:04 -0500

    forgot commas, oops sorry all :(

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -54,7 +54,7 @@ router.get("/wine", (req, res) => {
       "post_body",
       "created_at",
       "post_url",
-      "image_url"
+      "image_url",
       [
         sequelize.literal(
           "(SELECT COUNT(*) FROM upvote WHERE upvote.post_id=post.id)"
@@ -86,7 +86,7 @@ router.get("/spirits", (req, res) => {
       "post_body",
       "created_at",
       "post_url",
-      "image_url"
+      "image_url",
       [
         sequelize.literal(
           "(SELECT COUNT(*) FROM upvote WHERE upvote.post_id=post.id)"
@@ -115,7 +115,7 @@ router.get("/", (req, res) => {
       "post_body",
       "created_at",
       "post_url",
-      "image_url"
+      "image_url",
       [
         sequelize.literal(
           "(SELECT COUNT(*) FROM upvote WHERE upvote.post_id=post.id)"
