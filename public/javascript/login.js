5848cf20763e2f3ba308b4cb78209324e0088bac
lundbmp <m.lundberg008@gmail.com>
2022-08-09 20:33:42 -0500

    fixing login issue

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,14 +1,14 @@
 async function loginForm(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
-    if (username && password) {
+    if (email && password) {
         const response = await fetch ('/api/users/login', {
-            method: 'post',
+            method: 'POST',
             body: JSON.stringify({
-                username,
+                email,
                 password
             }),
             headers: {
